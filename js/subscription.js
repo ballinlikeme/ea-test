9b9c09fdce634e916323b7a7c948d39373085967
ballinlikeme <maxim.spirkin2003@gmail.com>
2023-02-18 20:16:47 +0300

    init: first commit

---
+++ js/subscription.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+     const button = document.getElementById('submit')
+     const input = document.getElementById('email')
+
+     const modal = document.getElementById('modal')
+     const modalError = document.getElementById('modal-error')
+     const errModalText = document.getElementById('err-modal-text')
+
+     const closeBtns = [...document.getElementsByClassName('close-modal')]
+     console.log(closeBtns)
+
+     const closeModal = () => {
+          modal.classList.remove('active')
+          modalError.classList.remove('active')
+     }
+
+     input.oninput = () => {
+          
+          input.checkValidity() 
+               ? button.disabled = false 
+               : button.disabled = true
+
+     }
+
+     button.onclick = async () => {
+          if (!button.disabled) {
+               const email = input.value
+               if (email === '') {
+                    return
+               }
+               try {
+                    const response = await fetch('https://jsonplaceholder.typicode.com/', {
+                         method: "POST",
+                         body: JSON.stringify({email})
+                    }) // Фейк апи вернет 404 статус код
+                    input.value = ''
+                    // if (response.status !== 200) throw new Error('Something went wrong')
+                    modal.classList.add('active')
+               } catch (error) {
+                    modalError.classList.add('active')
+                    errModalText.textContent = error.message
+               }
+          }
+     }
+
+     closeBtns.forEach(btn => btn.onclick = closeModal)
+
+})
---
+++ js/timer.js
@@ -0,0 +1,46 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+     const daysField = document.getElementById('days')
+     const hoursField = document.getElementById('hours')
+     const minutesField = document.getElementById('minutes')
+     const secondsField = document.getElementById('seconds')
+
+     const endDate = {
+          year: "2023",
+          month: "05",
+          day: "31",
+          hours: "00",
+          minutes: "00",
+          seconds: "00",
+     }
+
+     const endDateString = `${endDate.year}-${endDate.month}-${endDate.day}T${endDate.hours}:${endDate.minutes}:${endDate.seconds}`;
+
+     let timer = setInterval(() => {
+          const now = new Date()
+          const date = new Date(endDateString)
+          const left = date - now
+          if (left <= 0) return clearInterval(timer)
+          const result = new Date(left)
+          const monthsLeft = result.getUTCMonth()
+          let daysLeft = result.getUTCDate() - 1
+          let hoursLeft = result.getUTCHours()
+          let minutesLeft = result.getUTCMinutes()
+          let secondsLeft = result.getUTCSeconds()
+
+          if (monthsLeft > 0) {
+               
+               const currentMonth = new Date().getMonth()
+               const currentYear = new Date().getFullYear()
+
+               for (let i = 1; i <= monthsLeft; i++) {
+                    daysLeft += new Date(currentYear, currentMonth + i, 0).getDate()
+               }
+          }
+
+          daysField.textContent = daysLeft < 10 ? `0${daysLeft}` : daysLeft 
+          hoursField.textContent = hoursLeft < 10 ? `0${hoursLeft}` : hoursLeft
+          minutesField.textContent = minutesLeft < 10 ? `0${minutesLeft}` : minutesLeft
+          secondsField.textContent = secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft
+     }, 1000)
+})
